800651a2ee6aab186c71a39d7209ea33d2f64886
Matt Derrick <git@mattderrick.co.uk>
2015-10-28 21:27:48 +0000

    Improved development setup with hot reloading

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,30 +1,47 @@
 var path = require('path');
 var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     debug: true,
-    entry: './src/entry.js',
+    entry: {
+        app: [
+            './src/entry.js',
+            './src/assets/app.css'
+        ]
+    },
     target: 'web',
+    // Resolves are needed as we require a file that is
+    // in a parent directory and webpack can't find node_modules
+    // when we do this.
+    resolveLoader: {
+        root: path.join(__dirname, './node_modules')
+    },
+    resolve: {
+        root: path.join(__dirname, './node_modules')
+    },
     output: {
+        publicPath: '/dist/',
         path: path.join(__dirname, './dist'),
         filename: 'app.js',
         chunkFilename: '[id].js'
     },
     module: {
+        // Babel loader must be first as it's modified by server.js
         loaders: [{
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        }, {
             test: /\.js$/,
-            loader: 'babel'
+            // Must be an array as server.js adds 'react-hot' loader
+            loaders: ['babel'],
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
         }, {
             test: /\.(svg|woff([\?]?.*)|ttf([\?]?.*)|eot([\?]?.*)|svg([\?]?.*))$/i,
             loader: 'url?limit=10000'
         }]
     },
     plugins: [
-        new ExtractTextPlugin('ReactHtml5Video.css'),
+        // First two plugins are removed by server.js
         new webpack.optimize.UglifyJsPlugin({
             minimize: true,
             output: {
