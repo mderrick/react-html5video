5d1b529d336d88c8d5a3b68405416a294215fda5
Eddie Chen <yuchuan1@gmail.com>
2017-05-16 16:56:54 +0800

    Fix #88 PropTypes deprecation warning (#92)
    * Fix #88 PropTypes deprecation warning
    
    * move prop-types to devDependency

--- src/DefaultPlayer/DefaultPlayer.js
+++ src/DefaultPlayer/DefaultPlayer.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import videoConnect from './../video/video';
 import copy from './copy';
 import {
